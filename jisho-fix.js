e942ec473b7f5b959bf487d84cd9fbf2374a931e
slyborg <1388365+runrunrirun@users.noreply.github.com>
2022-11-13 01:27:20 -0600

    Updates for browser dark mode support

--- jisho-fix.js
+++ jisho-fix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jisho.org Jisho-modo
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Slightly rejigger jisho.org search results to improve info density
 // @author       slyborg
 // @match        https://jisho.org/search/*
@@ -12,19 +12,19 @@
 
 (function() {
     'use strict';
-  
+
   var css = `
     .row,
     .page {
         max-width: 100% !important;
     }
-    
+
     #search {
         max-width: 90% !important;
         margin-left: auto;
         margin-right: auto;
     }
-    
+
     #main_results {
         margin-top: -0.5em;
     }
@@ -41,9 +41,9 @@
     .result_count {
         font-weight: bold !important;
         font-size: 1.0em !important;
-        color: black !important;
+        color: gray !important;
     }
-    
+
     #primary,
     .concept_light-meanings,
     .sentence,
@@ -56,21 +56,21 @@
         margin-top: -8px !important;
         margin-bottom: -8px !important;
     }
-    
+
     #the_corrector {
         margin-bottom: 1px !important;
     }
-    
+
     .meaning-tags {
         margin-top: -7px !important;
         margin-bottom: -7px !important;
     }
-    
+
     .break-unit {
         display: inline-block;
         margin-top: 5px;
     }
-    
+
     /* meanings */
     .meaning-meaning {
         font-size: 1.1em !important;
@@ -103,15 +103,26 @@
     styleSheet.type = "text/css";
     styleSheet.innerText = css;
     document.head.appendChild(styleSheet);
-  
+
+    var darkMode = Boolean(false);
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        console.log("Dark mode detected");
+        darkMode = true;
+    }
+    else {
+        console.log("No Dark mode detected");
+    }
+
     const meaningTags = document.querySelectorAll("div.meaning-tags");
     const studylvlTags = document.querySelectorAll("div.concept_light-status > span");
-  
+
     // Number of words found in search
     document.querySelectorAll("h4").forEach(h => {
         if (h.innerHTML.startsWith("Words") || h.innerHTML.startsWith("Names") || h.innerHTML.startsWith("Sentences")) {
             h.style.fontSize = "1.0em";
             h.style.borderBottom = "1px solid #000000";
+            h.style.borderColor = "gray";
         }
     });
 
@@ -120,19 +131,19 @@
     jishohdr.forEach(s => {
         s.style.display = "none";
     });
-  
+
     // Set document container to use the actual page width instead of arbitrarily half of it because idk
     const container = document.querySelectorAll("page_container");
     container.forEach(s => {
         s.style.width = "100%";
     });
-  
+
     // Set primary result display to use full render width
     const Primary = document.querySelectorAll("div#primary");
     Primary.forEach(s => {
         s.setAttribute('class','large-12 columns')
     });
-  
+
     // Purge righthand displays
     const altright = document.querySelectorAll("div#secondary.large-4.columns.search-secondary_column");
     altright.forEach(s => {
@@ -144,8 +155,8 @@
     sentences.forEach(s => {
         s.style.display = "none";
     });
-  
-    // Parts of speech purge 
+
+    // Parts of speech purge
     meaningTags.forEach(s => {
         if( s.innerHTML.startsWith("Other") || (s.innerHTML === "Wikipedia definition")) {
           s.style.display = "inline";
@@ -184,7 +195,7 @@
     notes.forEach(note => {
         note.style.display = "none";
     });
-  
+
     meaningTags.forEach(tag => {
         if (tag.innerHTML === "Notes") {
             tag.style.display = "none";
@@ -200,17 +211,24 @@
             s.innerHTML = "Yojijukugo";
         }
     });
-    
+
     // Emphasize row numbers
     // You can change the number color every other row if desired
-    const numlabels = document.querySelectorAll("span.meaning-definition-section_divider");  
+    const numlabels = document.querySelectorAll("span.meaning-definition-section_divider");
     var iterator = 1;
-    
+    var rownumcolor = "black";
+    var altrownumcolor = "black"
+
+    if (darkMode) {
+      rownumcolor = "white";
+      altrownumcolor = "white";
+    };
+
     numlabels.forEach(lbl => {
         lbl.style.fontWeight = "normal";
-        lbl.style.color = "black";
+        lbl.style.color = rownumcolor;
         if ((iterator % 2) == 0) {
-            lbl.style.color = "black";
+            lbl.style.color = altrownumcolor;
         }
         iterator++;
     });
@@ -218,12 +236,17 @@
     // Make colored rows
     const defrow = document.querySelectorAll("div.concept_light.clearfix,li.entry.sentence.clearfix");
     var iterator2 = 1;
-  
+
     defrow.forEach(d => {
         if ((iterator2 % 2) == 0) {
+            if (darkMode) {
+            d.style.backgroundColor = "#0d47a1";
+            }
+          else {
             d.style.backgroundColor = "#effbff";
+          }
         }
         iterator2++;
     });
-  
+
 })();
